6e9276041a5924a7bc20d55bae0ad9a2cc3a8a5e
Audrey Lai <aflai@uci.edu>
2015-12-01 00:15:43 -0800

    Deleted Extraneous Files
    (see summary)

--- RPG_Town/SettingsMenu.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  SettingsMenu.cpp
-//  RPG_Town
-//
-//  Created by Audrey Lai on 11/23/15.
-//  Copyright (c) 2015 Audrey Lai. All rights reserved.
-//
-
-#include "SettingsMenu.h"
--- RPG_Town/TextButton.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  TextButton.cpp
-//  RPG_Town
-//
-//  Created by Audrey Lai on 11/23/15.
-//  Copyright (c) 2015 Audrey Lai. All rights reserved.
-//
-
-#include "TextButton.h"
