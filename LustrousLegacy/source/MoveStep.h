d34720f674b31e0b2d02830b94811841a5e3df01
afly6899 <aflai@uci.edu>
2016-03-08 13:13:33 -0800

    Just saving code before trying to use maploader to load events
    Planning to change maploader function and TMX file (details in next
    commit)

--- LustrousLegacy/source/MoveStep.h
+++ LustrousLegacy/source/MoveStep.h
@@ -9,7 +9,6 @@
 
 class MoveStep : public Step{
 public:
-
 	MoveStep(std::vector<sf::Vector2f> positions);
 	virtual ~MoveStep();
 	virtual bool run(float elapsedTime, Actor& actor);
@@ -25,3 +24,19 @@ private:
 	std::string type = "Move";
 };
 #endif
+
+//std::map<int, bool> test;
+//std::map<int, event> events;
+//// key is ID
+//if (test[0]) {
+//	runEvent(events[0]);
+//}
+//
+//std::map<int<std::vector<Events>> events
+//int counter = 0
+//if (outside_boolean_that_tells_event_to_happen) {
+//	for event in events[counter]{
+//	runEvent(event);
+//	}
+//	counter++;
+//}
